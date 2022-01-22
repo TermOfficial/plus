2f0a2bb1f03acc9e6df01637458f9229e2190ba3
TermOfficial <79934414+TermOfficial@users.noreply.github.com>
2022-01-22 12:24:19 -0500

    Update plus.js

--- plus.js
+++ plus.js
@@ -15,24 +15,31 @@ Please enjoy.
 
 // -- Config -- \\
 
-var dev = false;
-var build = 'b7';
+var dev = true;
+var rescuemode = false;
+var build = 'b8';
 var updaturl = 'https://raw.githubusercontent.com/TermOfficial/plus/main/b.txt';
+var addon = '';
 
 // Might make it so it runs off of the raw URL instead of the file.
 
 // -- End Config -- \\
-
+if(rescuemode == true){
+	console.warn("W96 Plus is currently running in recovery mode.")
+	console.warn("since we don't actually have a real recovery mode i'm just going to exit the script now.")
+} else {
 console.log("Windows 96 PLUS! is now starting up...")
 console.log("We are running build:", build)
 if(dev == true){
 console.warn("W96+ is running in dev mode. To disable this, please disable it in config.")
 console.warn("bypassing self check...")
 } else {
+    /*
+    broken
 function reqListener () {
   if (this.status==200)
 {
-      if(this.responseText == build) {
+      if(this.responseText !== build) {
           console.log("W96+ is up-to-date.")
       } else {
         console.warn("Your build of W96+ (plus.js) is out of date. Please update it.")
@@ -48,6 +55,7 @@ var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
 oReq.open("GET", updaturl);
 oReq.send();
+*/
     // -- Self Check -- \\
 const fs = w96.FS;
 
@@ -69,4 +77,5 @@ if(fs.exists(PATH) && fs.exists(EXEC)){
 // -- End Self Check -- \\
 
 // startup
+}
 }
