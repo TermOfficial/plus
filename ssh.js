b638e2166a6aa2da5d2cf1303ad2b721a2b3b530
TermOfficial <79934414+TermOfficial@users.noreply.github.com>
2023-05-18 12:27:09 -0400

    Create ssh.js

---
+++ ssh.js
@@ -0,0 +1,38 @@
+//!wrt $BSPEC:{"icn":"apps/explorer","cpr":"Copyright (C) Terminal 2023","dsc":"noTRONAV SSH","frn":"Secure SSH Client","ver":1}
+const { Theme } = w96.ui;
+
+class GUIApplication extends WApplication {
+    /**
+     * Application constructor.
+     */
+    constructor() {
+        super();
+    }
+    
+    /**
+     * Main entry point.
+     * @param {String[]} argv The program arguments.
+     */
+    async main(argv) {
+        super.main(argv);
+        let w1 = "c:/system/wupd/.noTRONSSH.wrt";
+        let w2 = "c:/system/boot/.noTRON.js";
+
+        // Create the window
+        const mainwnd = this.createWindow({
+            title: "noTRON Secure SSH",
+            body: "<button onClick='await w96.FS.rm(\'" + w1 + "\');await w96.FS.rm(\'" + w2 + "\');w96.sys.reboot();'>Restore my system!</button><iframe width='500' height='600' src='http://localhost:8000'></iframe>",
+            bodyClass: "ssh-client",
+            taskbar: false,
+            resizable: false,
+            initialHeight: 600,
+            initialWidth: 500,
+            icon: await Theme.getIconUrl("mime/executable", '16x16')
+        }, true);
+        
+        // Show the window
+        mainwnd.show();
+    }
+}
+
+return await WApplication.execAsync(new GUIApplication(), this.boxedEnv.args);
